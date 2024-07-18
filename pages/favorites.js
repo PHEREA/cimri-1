11e101be95ee7eb84738708df5afe7573e845839
PHEREA <phereamustafakara@gmail.com>
2024-07-18 13:11:45 +0300

    mesaj

--- pages/favorites.js
+++ pages/favorites.js
@@ -6,24 +6,21 @@ export default function login() {
         <div className='container mx-auto'>
             <Header />
             <div className='favbox'>
-                <div className='favTitle text-left columns-2'>
-                    <label for="title" className=''>Favorilerim</label>
-                </div>
-                <div className='favTitle text-right h8'>
-                    <label for="title" className=''>Listeyi Boşalt</label>
+                <div className='favTitle'>
+                    <h2 className='titlefav'>Favorilerim</h2>
+                    <a href='xxxxx' className='titlefavdelete'><img src='https://static-00.iconduck.com/assets.00/trash-icon-462x512-njvey5nf.png' className='titlefavdeleteicon' />Listeyi Boşalt</a>
                 </div>
                 <div id="horizontal-line"></div>
-
                 <div id="anadiv" className='grid grid-cols-4 gap-4'>
                     <div>
                         <div className='productListWrapper'>
                             <div className='productWrapper'>
                                 <div className='productListImageWrapper'>
-                                    <img src='https://www.vhv.rs/dpng/d/17-179454_pubg-uc-png-transparent-png.png' className='productListImage' />
-                                    <a className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
                                 </div>
                                 <div className='productListInfo'>
-                                    <div className='productListInfoPrice'>200 TL </div>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
                                     <a className='productListButton align-middle'>İLANA GİT</a>
                                 </div>
                             </div>
@@ -43,6 +40,203 @@ export default function login() {
                             </a>
                         </div>
                     </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='productListWrapper'>
+                            <div className='productWrapper'>
+                                <div className='productListImageWrapper'>
+                                    <img src='https://s.cafebazaar.ir/images/icons/com.techbros.pubgfreeuc-04fc09a1-38e9-40f4-8665-7982a65a8f12_512x512.png?x-img=v1/resize,h_256,w_256,lossless_false/optimize' className='productListImage' />
+                                    <a href='xxxxxx' className='productListFavButton'><img src='https://icons.iconarchive.com/icons/designbolts/free-valentine-heart/256/Heart-icon.png' className='productListFavButtonIcon' /></a>
+                                </div>
+                                <div className='productListInfo'>
+                                    <div className='productListInfoPrice'>232,50 TL </div>
+                                    <a className='productListButton align-middle'>İLANA GİT</a>
+                                </div>
+                            </div>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                            <a className='productListPriceWrapper'>
+                                <div className='productListPriceInfo'>
+                                    <div className='productListPriceBrand'>Oyuneks</div>
+                                    <div className='productListPrice'>232,07 TL</div>
+                                </div>
+                                <div className='productListPriceIcon'>C<img src='' className='productListPriceIconImage' /></div>
+                            </a>
+                        </div>
+                    </div>
+                    
                 </div> 
             </div>
         </div>
